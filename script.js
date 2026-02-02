b17aaf2d7f57a08d879c804a18f9cc281c2cdad8
Md Shariful Islam <shariful07cse@gmail.com>
2026-02-02 21:35:15 +0600

    oki

--- script.js
+++ script.js
@@ -6,19 +6,18 @@ const maxSeats = 4;
 
 for (const seat of allSeats) {
     seat.addEventListener('click', function() {
-        // If seat is already selected, remove it (deselect)
+        
         if (selectedSeats.includes(seat.id)) {
             removeSeat(seat.id);
             return;
         }
 
-        // Validation: Max 4 seats
+       
         if (selectedSeats.length >= maxSeats) {
             alert("You can only select up to 4 seats.");
             return;
         }
 
-        // Add Seat
         selectSeat(seat.id);
     });
 }
